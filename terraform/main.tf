f91ad40c55f14002441cd5b61332c9f536da08cf
Anton Holovin <anton.holovin@ibm.com>
2022-09-06 12:41:47 +0300

    Testing terraform

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_secret" "test_secret" {
+  metadata {
+    name = "basic-auth"
+  }
+
+  data = {
+    username = "admin"
+    password = "P4ssw0rd"
+  }
+
+  type = "kubernetes.io/basic-auth"
+}
