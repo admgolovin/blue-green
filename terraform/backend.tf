fd2fb7b11026dc69bc35c1aab0753e4f669101d9
Anton Holovin <anton.holovin@ibm.com>
2022-09-07 15:44:33 +0300

    Testing azure terraform auth

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "cloud-shell-storage-westeurope"
-    storage_account_name = "tfstate"
+    storage_account_name = "csb100320022a21011b"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
   }
